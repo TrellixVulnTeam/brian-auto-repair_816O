98056a7ace846e9e8377622c8db7c203efdabdc2
Nicksonder Examar <nextgendev12@gmail.com>
2021-10-30 21:09:38 -0400

    Finished making the website responsive, moving on to testing.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("hello World!!");
+// Mobile Menu
+let hamburger = document.querySelector(".header__hamburger");
+let mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("mobile-menu--active");
+});
+
+console.log(hamburger);
