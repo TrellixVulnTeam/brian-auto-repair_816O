a03cff766094294fb31ccbfe18ab2847646a866d
Nicksonder Examar <nextgendev12@gmail.com>
2021-11-05 22:54:06 -0400

    I'm done with the website, optimized it to work on all the major modern browsers.

--- js/index.js
+++ js/index.js
@@ -5,5 +5,3 @@ let mobileMenu = document.querySelector(".mobile-menu");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("mobile-menu--active");
 });
-
-console.log(hamburger);
